d72b489f4ab19b5b855ed17f77aee8dc99423da0
juanarancibia <arancibia.juan22@gmail.com>
2022-10-28 16:29:05 -0300

    Delete unnecessary code

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,11 @@
-import type { FC } from 'react'
+import type { FC } from 'react';
 
 const Header: FC = () => {
   return (
     <header className="mx-auto py-6 shadow-lg w-full text-center">
-      <h1 className="text-3xl font-bold">p5.js + Next.js + Typescript </h1>
+      <h1 className="text-3xl font-bold">Polkadot Hackathon </h1>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
